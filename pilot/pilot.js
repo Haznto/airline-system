0a41b4cc0ab2cc683ff24b0bfd60eb6b91942ad2
Haznto <haznto@gmail.com>
2023-07-10 10:57:34 +0000

    fixed dates

--- pilot/pilot.js
+++ pilot/pilot.js
@@ -8,12 +8,14 @@ function handleNewFlight(payload){
         console.log(`Pilot: flight with ID ‘${payload.Details.flightID}’ took-off
         `)
         payload.event = 'took-off';
+        payload.time = new Date();
         eventsEmitter.emit('took-off',payload)
     },4000)
     setTimeout(() => {
         console.log(`Pilot: flight with ID ‘${payload.Details.flightID}’ has arrived
         `)
         payload.event = 'Arrived';
+        payload.time = new Date()
         eventsEmitter.emit('Arrived',payload)
     },7000)
 }
